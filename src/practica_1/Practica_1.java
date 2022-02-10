d2fead315a003559ddf63decdb6c30d1aae33b2e
Ruler2798 <2995432420101@ingenieria.usac.edu.gt>
2022-02-09 22:15:12 -0600

    commit 1
    asd

--- src/practica_1/Practica_1.java
+++ src/practica_1/Practica_1.java
@@ -1,57 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package practica_1;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-/**
- *
- * @author lphg3
- */
 public class Practica_1 {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner lector = new Scanner(System.in);
-        boolean salir = false;
-        int opcion; 
- 
-        while (!salir) {
-            
-            System.out.println("1. Pig Latin");
-            System.out.println("2. Operaciones bitwise");
-            System.out.println("3. Salir");
- 
-            try {
- 
-                System.out.println("Escribe una de las opciones");
-                opcion = lector.nextInt();
- 
-                switch (opcion) {
-                    case 1:
-                      //pig_latin();
-                        break;
-                    case 2:
-                        //Operaciones_bitwise();
-                        break;
-                    case 3:
-                        salir = true;
-                        break;
-                    default:
-                        System.out.println("Solo números entre 1 y 3");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Debes insertar un número");
-                lector.next();
-            }
-      }
+        menu_general menu1 = new menu_general();
+        menu1.menu();
         
     }
     
--- src/practica_1/menu_general.java
+++ src/practica_1/menu_general.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package practica_1;
 
-/**
- *
- * @author lphg3
- */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class menu_general {
-    
+     
+    public void menu () {
+      Scanner lector = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; 
+ 
+        while (!salir) {
+            
+            System.out.println("1. Iniciar Juego");
+            System.out.println("2. Ver ultimas puntuaciones");
+            System.out.println("3. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = lector.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        //tablero();
+                        break;
+                    case 2:
+                        //puntajes();
+                        break;
+                    case 3:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 3");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                lector.next();
+            }
+      }
+}
 }
